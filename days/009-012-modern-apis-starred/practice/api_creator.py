93a39830312400bd709aa30c181aab4b1bbd478d
Qkessler <qkessler@gmail.com>
2020-05-14 09:49:03 +0200

    Adding not working post.

--- days/009-012-modern-apis-starred/practice/api_creator.py
+++ days/009-012-modern-apis-starred/practice/api_creator.py
@@ -1,6 +1,7 @@
 import json
 from typing import List
 import csv
+from datetime import datetime
 from pprint import pprint as pp
 from apistar import App, Route, types, validators
 from bs4 import BeautifulSoup
@@ -10,11 +11,11 @@ import requests
 
 
 headers= 'id,created,topic,question,answer'.split(',')
-data = {}
+questions = {}
 
 
 def load_csv():
-    data.clear()
+    questions.clear()
     with open('questions.csv', 'r') as f:
         csv_data = csv.DictReader(f)
         for row in csv_data:
@@ -22,8 +23,8 @@ def load_csv():
             for header in headers:
                 h = row[header]
                 info[header]= h
-            data[row['id']] = info
-    return data
+            questions[row['id']] = info
+    return questions
 
 
 class Question(types.Type):
@@ -37,10 +38,13 @@ class Question(types.Type):
 def list_questions(language):
     if language in LANGCODES.values():
         translator = Translator()
-        return_data = [Question(id=int(question[1].get('id')), created=question[1].get('created'),
+        return_data = [Question(id=int(question[1].get('id')),
+                                created=question[1].get('created'),
                                 topic=int(question[1].get('topic')),
-                                question=translator.translate(question[1].get('question'), dest=language).text,
-                                answer=translator.translate(question[1].get('answer'), dest=language).text)
+                                question=translator.translate(question[1].get('question'),
+                                                              dest=language).text,
+                                answer=translator.translate(question[1].get('answer'),
+                                                            dest=language).text)
                        for question in data.items()]
         return JSONResponse(return_data, status_code=200)
     else:
@@ -55,9 +59,31 @@ def test_language_translation(language, string):
         return None
 
     
-def create_question():
-    pass
+def create_question(question):
+    return JSONResponse(question, status_code=400)
+    question_id = max(questions.keys()) + 1
+    question.id = question_id
+    question.created = datetime.now()
+    questions[question_id] = question
+    return JSONResponse(Question(question), status_code=201)
 
+
+def post_question():
+    question = Question(id=1,
+                        created=datetime.now(),
+                        topic=2,
+                        question='Esto es una prueba de pregunta',
+                        answer='Esto es una prueba de respuesta')
+    q_json = {}
+    q_json['id'] = question.id
+    q_json['created'] = question.created
+    q_json['topic'] = question.topic
+    q_json['question'] = question.question
+    q_json['answer'] = question.answer
+    response = requests.post('http://127.0.0.1:5000/', data=q_json)
+    print(response.status_code)
+    return response.json()
+    
 
 def details_question():
     pass
@@ -65,15 +91,13 @@ def details_question():
 
 routes = [
     Route('/{language}/', method='GET', handler=list_questions),
-    Route('/{language}/', method='POST', handler=create_question),
+    Route('/', method='POST', handler=create_question),
     Route('/{language}/{question_id}/', method='GET', handler=details_question)
 ]
 
 app = App(routes=routes)
 
 
-if __name__ == '__main__':
-    # print(LANGCODES)
-    # print(test_language_translation('fr','This is english'))
+if __name__ == '__main__':    
     load_csv()
     app.serve('127.0.0.1', 5000, debug=True)
